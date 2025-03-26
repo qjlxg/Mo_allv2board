efd17e82384f88d0f053953c0e2280ef4409d1dd
qjlxg <qjlxg@163.com>
2025-03-26 11:05:30 +0800

    Update collect.py

--- subscribe/collect.py
+++ subscribe/collect.py
@@ -573,7 +573,7 @@ if __name__ == "__main__":
         type=int,
         required=False,
         #default=sys.maxsize,
-        default=50000,
+        default=90000,
         help="max page number when crawling telegram",
     )#抓取电报时的 ax 页码
 
